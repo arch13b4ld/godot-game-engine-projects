f051f83fe921908b392bc5c28cc170b19dfd5b27
Frank David Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-16 11:46:24 -0400

    Add Player script with initial base configuration

---
+++ 02-coin-dash/Player.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export (int) var speed
+
+var velocity 	= Vector2()
+var screensize 	= Vector2(480, 720)
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
