545cb93d4646f8d485eb762ab89156405db72a0c
Frank D. Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-18 15:43:44 -0400

    Add input handling for player movement

---
+++ 03-escape-the-maze/characters/player/Player.gd
@@ -0,0 +1,15 @@
+extends "res://characters/Character.gd"
+
+signal moved
+
+func _ready():
+	pass
+
+func _process(delta):
+	if can_move:
+		for dir in moves.keys():
+			if Input.is_action_pressed(dir):
+				if move(dir):
+					emit_signal("moved")
+
+
