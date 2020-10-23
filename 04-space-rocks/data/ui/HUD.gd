f9a9c834d7a5f9894df483ccab21a40219479870
Frank David Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-23 15:36:25 -0400

    Updated display control in HUD scene

--- 04-space-rocks/data/ui/HUD.gd
+++ 04-space-rocks/data/ui/HUD.gd
@@ -4,6 +4,11 @@ signal start_game
 
 var lives_counter = []
 
+func game_over():
+	show_message("Game Over :(")
+	yield($TimerMessage, "timeout")
+	$ButtonStart.show()
+
 func update_lives(value):
 	for life in range(lives_counter.size()):
 		lives_counter[life].visible = value > life
@@ -15,6 +20,14 @@ func show_message(message):
 	$LabelMessage.text = message
 	$LabelMessage.show()
 	$TimerMessage.start()
+
+func _on_TimerMessage_timeout():
+	$LabelMessage.text = ""
+	$LabelMessage.hide()
+
+func _on_ButtonStart_pressed():
+	$ButtonStart.hide()
+	emit_signal("start_game")
 
 func _ready():
 	lives_counter = [
