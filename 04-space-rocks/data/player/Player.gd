8fa5934c384b6ebc773195682c85397ac1ca2fbc
Frank D. Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-22 23:14:24 -0400

    Added input handling to Player scene

--- 04-space-rocks/data/player/Player.gd
+++ 04-space-rocks/data/player/Player.gd
@@ -11,8 +11,22 @@ export (int) var engine_power
 export (int) var spin_power
 
 var state = null
-var thrust = Vector2()
-var rotation_dir = 0
+var thrust
+var rotation_dir
+
+func handle_input():
+	thrust = Vector2()
+
+	if state in [State.INIT, State.DEAD]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = Vector2(engine_power, 0)
+
+	rotation_dir = 0
+	if Input.is_action_pressed("rotate_right"):
+		rotation_dir += 1
+	elif Input.is_action_pressed("rotate_left"):
+		rotation_dir -= 1
 
 func set_state(new_state):
 	match new_state:
@@ -26,6 +40,9 @@ func set_state(new_state):
 			$CollisionShape2D.disabled = true
 
 	state = new_state
+
+func _process(delta):
+	handle_input()
 
 func _ready():
 	set_state(State.INIT)
