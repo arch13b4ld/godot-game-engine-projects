63370dd71b71aae96a006621a88b632c2b9344a9
Frank David Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-23 10:59:07 -0400

    Added shotting control to Player scane

--- 04-space-rocks/data/player/Player.gd
+++ 04-space-rocks/data/player/Player.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal shoot
+
 enum State {
 	INIT,
 	ALIVE,
@@ -9,17 +11,30 @@ enum State {
 
 export (int) var engine_power
 export (int) var spin_power
+export (PackedScene) var Bullet
+export (float) var fire_rate
 
 var state = null
 var thrust = Vector2()
 var rotation_dir = 0
 var screensize = Vector2()
+var shootable = true
+
+func shoot():
+	if state == State.INVULNERABLE:
+		return
+
+	emit_signal("shoot", Bullet, $PositionGun.global_position, rotation)
+	shootable = false
+	$TimerGun.start()
 
 func handle_input():
 	thrust = Vector2()
 
 	if state in [State.INIT, State.DEAD]:
 		return
+	if Input.is_action_pressed("shoot") and shootable:
+		shoot()
 	if Input.is_action_pressed("thrust"):
 		thrust = Vector2(engine_power, 0)
 
@@ -41,6 +56,9 @@ func set_state(new_state):
 			$CollisionShape2D.disabled = true
 
 	state = new_state
+
+func _on_TimerGun_timeout():
+	shootable = true
 
 func _integrate_forces(physics_state):
 	set_applied_force(thrust.rotated(rotation))
@@ -65,3 +83,4 @@ func _process(delta):
 func _ready():
 	set_state(State.ALIVE)
 	screensize = get_viewport().get_visible_rect().size
+	$TimerGun.wait_time = fire_rate
