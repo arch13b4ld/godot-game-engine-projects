11c5a9d9f86ffccc34ff74cde47bb3c9e77134ca
Frank D. Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-25 14:11:53 -0400

    Added path and enemy movement to Enemie scene

--- 04-space-rocks/data/enemies/Enemie.gd
+++ 04-space-rocks/data/enemies/Enemie.gd
@@ -1,7 +1,32 @@
 extends Area2D
 
+signal shoot
+
+export (PackedScene) var Bullet
+export (int) var speed
+export (int) var health
+
+var follow
+var target = null
+
+func _on_TimerGun_timeout():
+	pass
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
 
+func _process(delta):
+	follow.offset += speed * delta
+	position = follow.global_position
+
+	if follow.unit_offset > 1:
+		queue_free()
+
 func _ready():
-	pass
+	$Sprite.frame = randi() % ($Sprite.vframes * $Sprite.hframes)
+
+	follow = PathFollow2D.new()
+	follow.loop = false
+
+	var path = $EnemyPaths.get_children()[randi() % $EnemyPaths.get_child_count()]
+	path.add_child(follow)
