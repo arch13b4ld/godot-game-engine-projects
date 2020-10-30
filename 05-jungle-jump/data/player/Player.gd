599075514e7c80536dbca86d0d1c4be4f2c52345
Frank David Pérez Alvares <arch13b4ld@protonmail.com>
2020-10-30 13:51:39 -0400

    Added Player finite state machine

---
+++ 05-jungle-jump/data/player/Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+enum State {
+	IDLE,
+	RUN,
+	JUMP,
+	CLIMB,
+	CROUCH,
+	HURT,
+	DEAD
+}
+
+var state
+var anim
+var new_anim
+
+func set_state(new_state):
+	state = new_state
+	
+	match state:
+		State.IDLE:
+			new_anim = 'idle'
+		State.RUN:
+			new_anim = 'run'
+		State.HURT:
+			new_anim = 'hurt'
+		State.JUMP:
+			new_anim = 'jump_up'
+		State.DEAD:
+			hide()
+
+func _physics_process(delta):
+	if new_anim != anim:
+		anim = new_anim
+		$Sprite/AnimationPlayer.play(anim)
+
+func _ready():
+	set_state(State.IDLE)
