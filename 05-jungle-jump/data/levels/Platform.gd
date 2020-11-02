8f8ff3cbe76664644dd8d4e4806fd0076cc5baad
Frank David Pérez Alvares <arch13b4ld@protonmail.com>
2020-11-02 14:56:08 -0500

    Added Platform movement

---
+++ 05-jungle-jump/data/levels/Platform.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+export (Vector2) var velocity
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+
+func _ready():
+	pass
